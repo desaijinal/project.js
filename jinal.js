6cfd4ea858cd592e773b91f52d641793d4c373e3
Jinal Desai <159759486+desaijinal@users.noreply.github.com>
2024-09-30 15:52:17 +0530

    Add files via upload

---
+++ jinal.js
@@ -0,0 +1,40 @@
+
+
+    // let a=10;
+    // let b=20;
+    // console.log(a+b);
+
+//   let x=50;
+//   let y=25;
+//   console.log(x-y);
+
+// let m=6;
+// let n=7;
+// console.log(m*n);
+
+// let p=20;
+// let q=4;
+// console.log(p/q);
+
+// let num1=15;
+// let num2=4;
+// console.log(num1 % num2);
+
+// let base=3;
+// let exponent=4;
+// console.log(base**exponent);
+
+// let a=10;
+// let b=15;
+// console.log(a>b);
+
+// let x=25;
+// let y=25;
+// console.log(x<=y);
+
+// let num =5;
+// console.log(num == '5');
+
+let num =5;
+console.log(num === '5');
+
